d3d7b69e2b67d2d4006d81581e43894a09e7954f
marshuxpenn <marshalx@seas.upenn.edu>
2022-04-23 03:34:52 -0400

    Fixed bugs

--- backend/routes/profile.routes.js
+++ backend/routes/profile.routes.js
@@ -4,7 +4,7 @@ const lib = require("../controllers/profile.controllers");
 router.post("/:username1/:username2", async (req, res) => {
   try {
     const results = await lib.friendStatus(req.params.username1, req.params.username2);
-    res.status(200).json({ data: results });
+    res.status(200).json({ status: results });
   } catch (err) {
     res.status(500).json({ error: "try again later" });
   }
@@ -30,7 +30,7 @@ router.delete("/friendRequest/:username1/:username2", async (req, res) => {
 
 router.post("/friend/:username1/:username2", async (req, res) => {
   try {
-    await lib.deleteFriend(req.params.username1, req.params.username2);
+    await lib.addFriend(req.params.username1, req.params.username2);
     res.status(200).json({ success: true });
   } catch (err) {
     res.status(500).json({ error: "try again later" });
@@ -39,7 +39,7 @@ router.post("/friend/:username1/:username2", async (req, res) => {
 
 router.delete("/friend/:username1/:username2", async (req, res) => {
   try {
-    await lib.addFriend(req.params.username1, req.params.username2);
+    await lib.deleteFriend(req.params.username1, req.params.username2);
     res.status(200).json({ success: true });
   } catch (err) {
     res.status(500).json({ error: "try again later" });
