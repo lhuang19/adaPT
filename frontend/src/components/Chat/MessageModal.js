c9cfbf800c61b7c3f431f42b94fe9ab2780dd78a
Sina Shaikh <68874240+SinaShaikh@users.noreply.github.com>
2022-04-22 23:59:46 -0400

    Create MessageModal.js

---
+++ frontend/src/components/Chat/MessageModal.js
@@ -0,0 +1,101 @@
+import React, { useState, useContext } from "react";
+import { Tooltip, Button, Modal, Form, Input } from "antd";
+import { FormOutlined } from "@ant-design/icons";
+import { postMessage } from "../../modules/storage";
+import { AuthUserContext } from "../../context/Auth";
+
+function MessageModal({ fetchNewMessages }) {
+  const [show, setShow] = useState(false);
+  const [form] = Form.useForm();
+  const { credentials } = useContext(AuthUserContext);
+
+  async function onCreate(values) {
+    const messageData = values;
+    messageData.time = Date.now();
+    await postMessage(credentials.username, messageData);
+    setShow(false);
+    fetchNewMessages();
+  }
+
+  return (
+    <>
+      <Modal
+        title="Make a Message"
+        centered
+        visible={show}
+        okText="Send"
+        onOk={() => {
+          form
+            .validateFields()
+            .then((values) => {
+              form.resetFields();
+              onCreate(values);
+            })
+            .catch((info) => {
+              alert("Validate Failed:", info);
+            });
+        }}
+        onCancel={() => setShow(false)}
+        width={1000}
+      >
+        <Form
+          form={form}
+          name="basic"
+          autoComplete="off"
+          style={{ margin: "auto", width: " 60% " }}
+          layout="vertical"
+        >
+          <Form.Item
+            label="To:"
+            name="recipient"
+            rules={[
+              {
+                required: true,
+                message: "Please input a recipient!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Title"
+            name="title"
+            rules={[
+              {
+                required: true,
+                message: "Please input a title!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Body"
+            name="body"
+            rules={[
+              {
+                required: true,
+                message: "Please input a body!",
+              },
+            ]}
+          >
+            <Input.TextArea />
+          </Form.Item>
+        </Form>
+      </Modal>
+      <div style={{ position: "fixed", bottom: "40px", right: "40px" }}>
+        <Tooltip title="Message">
+          <Button
+            type="primary"
+            shape="circle"
+            icon={<FormOutlined />}
+            size="large"
+            onClick={() => setShow(true)}
+          />
+        </Tooltip>
+      </div>
+    </>
+  );
+}
+
+export default MessageModal;
