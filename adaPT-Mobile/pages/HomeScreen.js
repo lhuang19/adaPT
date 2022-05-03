85b2e671d9f0c08980934744aa3648d0792ff52b
Connor Shu <12125674+cbxshu@users.noreply.github.com>
2022-05-03 18:07:15 -0400

    Fix lint errors

--- adaPT-Mobile/pages/HomeScreen.js
+++ adaPT-Mobile/pages/HomeScreen.js
@@ -5,7 +5,7 @@ export default function HomeScreen({ userData }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', backgroundColor: 'white' }}>
       <Text style={{ margin: 50 }}>
-        Hello, {userData.firstname} {userData.lastname}
+        {`Hello, ${userData.firstname} ${userData.lastname}`}
       </Text>
     </View>
   );
