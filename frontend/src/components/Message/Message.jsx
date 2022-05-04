9f07b2eb57e90c1ead2e3fcf0a685d46fb5291ce
SinaShaikh <68874240+SinaShaikh@users.noreply.github.com>
2022-05-03 23:31:48 -0400

    right and left

--- frontend/src/components/Message/Message.jsx
+++ frontend/src/components/Message/Message.jsx
@@ -1,45 +1,87 @@
-import React from "react";
+import React, {useContext} from "react";
 import { Avatar } from "antd";
 import { UserOutlined } from "@ant-design/icons";
+import { AuthUserContext } from "../../context/Auth";
+
 
 function Message(props) {
   const { data } = props;
   const { body, time, sender, senderFirstname } = data;
-
-  return (
-    <div
-      style={{
-        maxWidth: "80vw",
-        width: "100%",
-        padding: "20px",
-      }}
-    >
+  const { credentials } = useContext(AuthUserContext);
+  const { username } = credentials;
+  
+  if(username != sender){
+    return (
       <div
         style={{
-          display: "flex",
-          justifyContent: "left",
-          alignItems: "center",
-          paddingBottom: "1vw",
+          maxWidth: "80vw",
+          width: "100%",
+          padding: "20px",
         }}
       >
-        <Avatar
-          size={64}
-          icon={<UserOutlined />}
-          alt={sender.concat("'s profile picture")}
-          src={`https://joeschmoe.io/api/v1/${sender}`}
-        />
         <div
           style={{
-            paddingLeft: "1vw",
+            display: "flex",
+            justifyContent: "left",
+            alignItems: "center",
+            paddingBottom: "1vw",
           }}
         >
-          <p>{senderFirstname}</p>
-          <p>{new Date(time).toLocaleString()}</p>
+          <Avatar
+            size={64}
+            icon={<UserOutlined />}
+            alt={sender.concat("'s profile picture")}
+            src={`https://joeschmoe.io/api/v1/${sender}`}
+          />
+          <div
+            style={{
+              paddingLeft: "1vw",
+            }}
+          >
+            <p>{senderFirstname}</p>
+            <p>{new Date(time).toLocaleString()}</p>
+          </div>
         </div>
+        <p style={{ whiteSpace: "pre-wrap" }}>{body}</p>
       </div>
-      <p style={{ whiteSpace: "pre-wrap" }}>{body}</p>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div
+        style={{
+          maxWidth: "80vw",
+          width: "100%",
+          padding: "20px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "right",
+            alignItems: "center",
+            paddingBottom: "1vw",
+          }}
+        >
+          <Avatar
+            size={64}
+            icon={<UserOutlined />}
+            alt={sender.concat("'s profile picture")}
+            src={`https://joeschmoe.io/api/v1/${sender}`}
+          />
+          <div
+            style={{
+              paddingLeft: "1vw",
+            }}
+          >
+            <p>{senderFirstname}</p>
+            <p>{new Date(time).toLocaleString()}</p>
+          </div>
+        </div>
+        <p align = "right" style={{ whiteSpace: "pre-wrap"}}>{body}</p>
+      </div>
+    );
+  }
+  
 }
 
 export default Message;
