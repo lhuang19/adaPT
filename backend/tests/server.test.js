aa4c503fb5ec9ed357069cff800e83e5fee7d3ee
lhuang19 <70118730+lhuang19@users.noreply.github.com>
2022-05-06 23:49:11 -0400

    fix test

--- backend/tests/server.test.js
+++ backend/tests/server.test.js
@@ -5,6 +5,6 @@ const app = require("../server");
 describe("/ endpoint tests", () => {
   test("GET / endpoint status code and response 404", async () =>
     request(app).get("/somethinghellarandom").expect(404));
-  test("GET /api-docs.json endpoint status code and response 200", async () =>
-    request(app).get("/api-docs.json").expect(200));
+  test("GET /api/api-docs.json endpoint status code and response 200", async () =>
+    request(app).get("/api/api-docs.json").expect(200));
 });
